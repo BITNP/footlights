089d862448f08b5346ba4ecdbaff13203a1356c9
Feng Kaiyu <loveress01@outlook.com>
2023-01-15 21:14:42 +0800

    test(adt): trim all whitespaces in XML tree.

--- src/adt/tests.rs
+++ src/adt/tests.rs
@@ -19,13 +19,28 @@ pub fn compare_svg_text(left: &str, right: &str) -> Result<()> {
 }
 
 pub fn compare_svg(left: &Element, right: &str) -> Result<()> {
-    let right_root = elementtree::Element::from_reader(right.as_bytes()).unwrap();
+    let mut right_root: Element = elementtree::Element::from_reader(right.as_bytes()).unwrap();
+    delete_all_whitespaces(&mut right_root);
 
     let left_text = left.to_string()?;
     let right_text = right_root.to_string()?;
     assert_eq!(left_text, right_text);
 
     Ok(())
+}
+
+pub fn delete_all_whitespaces(tree: &mut Element) {
+    if !tree.text().is_empty() {
+        tree.set_text(tree.text().trim().to_string());
+    }
+
+    if !tree.tail().is_empty() {
+        tree.set_tail(tree.tail().trim().to_string());
+    }
+
+    for child in tree.children_mut() {
+        delete_all_whitespaces(child);
+    }
 }
 
 #[test]
